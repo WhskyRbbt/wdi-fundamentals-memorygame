ca893cfa6c5fb411d6221e5fc0078f6458583544
Jonathan <scces.2012@gmail.com>
2019-12-31 19:54:41 -0800

    created game logic

--- memorygame (my version)/js/main.js
+++ memorygame (my version)/js/main.js
@@ -1,6 +1,26 @@
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "king";
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardOne = cards[0];
+var cardTwo = cards[3];
 
-console.log("user flipped " + cardFour)
+var cardsInPlay = [];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+//if (cardsInPlay[]) {
+//  alert("You've found a match!");
+//} else {
+// alert("You didn't get it this time. Try again")
+//}
+// let function = "createBoard"
+// createBoard
+console.log("user flipped " + cardOne);
+console.log("user flipped " + cardTwo);
+if(cardsInPlay.length === 2) {
+
+	if(cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");	
+	}
+}
